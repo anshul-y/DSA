bab4f5c53f71ad4e18af62b51c4dac4b26a97dc8
Anshul Yadav <mail2anshulyadav@gmail.com>
2024-03-16 13:11:01 +0530

    Nodes class

---
+++ com/dsa/ListNode.java
@@ -0,0 +1,17 @@
+package com.dsa;
+
+public class ListNode {  
+    private int data;
+    public ListNode next = null;
+    public ListNode previous = null;
+
+    public ListNode(int data) {  
+        this.data = data;  
+        this.next = null;  
+        this.previous = null;  
+    }
+  
+    public int getData(){
+        return data;
+    }
+}  
---
+++ com/dsa/TreeNode.java
@@ -0,0 +1,15 @@
+package com.dsa;
+
+public class TreeNode {
+
+    public TreeNode leftNode;
+    public TreeNode rightNode;
+    public int data;
+
+    public TreeNode(int data){
+        this.data = data;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+
+}
