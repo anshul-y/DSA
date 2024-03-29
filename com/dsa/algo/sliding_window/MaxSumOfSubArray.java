3d7ca442aaa3f7754e203242ade518173a778e31
Anshul Yadav <mail2anshulyadav@gmail.com>
2024-03-29 18:06:29 +0530

    Max sum of sub array

--- com/dsa/algo/sliding_window/MaxSumOfSubArray.java
+++ com/dsa/algo/sliding_window/MaxSumOfSubArray.java
@@ -6,24 +6,37 @@ public class MaxSumOfSubArray {
 
     public static void main(String[] args) {
         int[] arr = {2, 7, 3, 5, 8, 1};
-        int maxSumSubArray = maxSumOfSubArray(arr, 3);
+        int maxSumSubArray = maxSumOfSubArray(arr, 2);
         Answer.print(maxSumSubArray, "Maximum sum of sub arrays of size " + 3);
     }
 
+    /**
+     * Finds the maximum sum of a subarray of length k in the given array.
+     *
+     * @param nums The input array of integers.
+     * @param k    The length of the subarray.
+     * @return The maximum sum of the subarray of length k.
+     */
     private static int maxSumOfSubArray(int[] nums, int k) {
-        int maxSum = Integer.MIN_VALUE;
-        int windowStart = 0;
-        int windowEnd = 0;
-        int windowSize = k;
-        for (int i = 0; i <= nums.length - 1; i++) {
-            if (windowEnd == windowSize) {
-                windowEnd = 0;
+        // Initialize variables to store the maximum sum, current window sum, start index, and end index
+        int maxSum = Integer.MIN_VALUE; // Initialize the maximum sum to the smallest possible integer value
+        int windowSum = 0; // Initialize the current window sum to 0
+        int start = 0; // Initialize the start index of the window
+        int end = 0; // Initialize the end index of the window
+
+        while (end < nums.length) { // Iterate through the array
+            windowSum += nums[end]; // Add the current element to the window sum
+            if (end >= k - 1) { // Check if the window size is equal to k
+                windowSum -= nums[start++];     // Remove the first element of the window from the window sum
+                maxSum = Math.max(maxSum, windowSum);   // Update the maximum sum by comparing it with the current window sum
             }
-            windowEnd++;
+            end++;  // Move the end pointer to the next element
         }
-        return maxSum;
+        return maxSum;  // Return the maximum sum
     }
 
+
+    // Time complexity O(n^2)
 //    private static int maxSumOfSubArray(int[] nums, int k) {
 //        int maxSum = Integer.MIN_VALUE;
 //        for (int i = 0; i <= nums.length - k; i++){
